b1819cdad34e47f0c480b79be68206ba9a1b0910
SEONGJOON KIM <jobcho6320@naver.com>
2022-08-01 02:17:00 +0900

    JS Practice

---
+++ app.js
@@ -0,0 +1,33 @@
+// const player = {
+//     name : "nico",
+//     age: 98,
+// }
+// console.log(player)
+
+// player.sexy = "soon";
+// console.log(player)
+
+// function plus() {
+//     console.log(2+2);   
+// }
+// plus();
+
+// alert("lalalala");
+
+
+
+const calculator = {
+    plus : function(a,b) {
+        console.log(a+b);
+    },
+    minus : function(a, b) {
+        console.log(a-b);
+    },
+    times : function (a,b) {
+        console.log(a*b);
+    },
+};
+
+calculator.plus(2,3);
+calculator.minus(2,3);
+calculator.times(2,3);
