52113cde8ae8e182ec6adab408b004b1e03128f2
SEONGJOON KIM <jobcho6320@naver.com>
2022-08-03 23:29:00 +0900

    js practice

--- app2.js
+++ app2.js
@@ -18,7 +18,11 @@ const calculator = {
     plus : function(a,b) {
         return a+b;
     },
+    minus : function(a,b) {
+        return a-b;
+    },
 };
 
 
 console.log(calculator.plus(231,432))
+console.log(calculator.minus(123,22))
