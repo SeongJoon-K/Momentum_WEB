548716ba0e667f81c34885995369d433410e268e
SEONGJOON KIM <jobcho6320@naver.com>
2022-08-09 02:14:21 +0900

    Vinila JS 3-3

--- app2.js
+++ app2.js
@@ -1,28 +1,11 @@
-// const age = prompt("How old are you");
-// prompt 사용시, 답을 입력 전 까지 실행을 멈춘다
-// prompt 의 어떠한 값도 js 의 value 로 가져가게 된다
+const title = document.querySelector("div.hello:first-child h1");
 
-const age = parseInt(prompt("How old are you?"));
+console.dir(title);
 
-if (isNaN(age) || age < 0){
-    console.log("please");
-} else if(age < 18) {
-    console.log("you");
-} else if(age >= 18 && age <= 50) {
-    console.log("you can");
-} else {
 
+function handleTitleClick() {
+    title.style.color = "blue";
 }
 
-const calculator = {
-    plus : function(a,b) {
-        return a+b;
-    },
-    minus : function(a,b) {
-        return a-b;
-    },
-};
+title.addEventListener("click", handleTitleClick)
 
-
-console.log(calculator.plus(231,432))
-console.log(calculator.minus(123,22))
