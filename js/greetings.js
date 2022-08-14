321fc11c96ecb9f8774ca09b3d6f34147f023843
SEONGJOON KIM <jobcho6320@naver.com>
2022-08-14 15:19:12 +0900

    Vinila JS 5-3

--- js/greetings.js
+++ js/greetings.js
@@ -8,9 +8,9 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
     event.preventDefault(); // 1. 브라우저의 기본동작을 막는다.
     loginForm.classList.add(HIDDEN_CLASSNAME) // 2. loginForm에서 hidden class 를 추가함.
-    const username = loginInput.value; // username이라는 변수를 
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings();
+    const username = loginInput.value; // username이라는 변수를 저장
+    localStorage.setItem(USERNAME_KEY, username); // 로컬스토리지에 등록함
+    paintGreetings(); // 함수를 실행함
     // 그렇게 되면 hidden이라는 클래스를 갖고 있는 것은 Form만 존재하게 됨
 }
 
